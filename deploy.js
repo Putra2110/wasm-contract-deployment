6a400e376a518822e0a6006b52617e5b6b677b62
Putra2110 <128735773+Putra2110@users.noreply.github.com>
2023-03-28 13:57:40 +0700

    node deploy.js

--- deploy.js
+++ deploy.js
@@ -31,7 +31,7 @@ try {
   
   
   // adding fire account for paying the gas fee
-  const PHRASE = 'negative cheap cherry uncover absurd angle swarm armor tuna lounge hurdle lawsuit';
+  const PHRASE = 'fix rib rural soon smooth animal swamp wheat chaos nerve state uncover';
   const seedUser = mnemonicToMiniSecret(PHRASE);
   const keyring = new Keyring({ type: "ed25519" });
   const userKeyring = keyring.addFromPair(naclKeypairFromSeed(seedUser));
